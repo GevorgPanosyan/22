3e5fea4c962042c448d9736898c36c986e865830
GevorgPanosyan <117856136+GevorgPanosyan@users.noreply.github.com>
2023-09-27 18:56:31 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,11 +1,15 @@
-﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число
- и на выходе показывает вторую цифру этого числа.
-456 -> 5
-782 -> 8
-918 -> 1 */
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6*/
 
-Console.WriteLine("Введи трёхзначное число: ");
-int threeNumber = Convert.ToInt32(Console.ReadLine());
-string stringNumber = Convert.ToString(threeNumber);
-Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
-
+Console.Write("Введи число: ");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberText = Convert.ToString(anyNumber);
+if (anyNumberText.Length > 2){
+  Console.WriteLine("третья цифра -> " + anyNumberText[2]);
+}
+else {
+  Console.WriteLine("Третьей цифры нет");
+}
