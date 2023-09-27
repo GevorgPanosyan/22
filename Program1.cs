a3642e5600c27c361798d21feb05821c046cbe53
GevorgPanosyan <117856136+GevorgPanosyan@users.noreply.github.com>
2023-09-27 19:03:33 +0300

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,11 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+ и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+Console.WriteLine("Введи трёхзначное число: ");
+int threeNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(threeNumber);
+Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
+
